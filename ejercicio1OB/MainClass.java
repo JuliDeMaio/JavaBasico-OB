36cd1553e94892f5354185cecd5a48d06f2ef35a
Julian De Maio <122990177+JuliDeMaio@users.noreply.github.com>
2023-02-26 23:17:37 -0300

    Add files via upload

---
+++ ejercicio1OB/MainClass.java
@@ -0,0 +1,25 @@
+package ejercicio1OB;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        String nombre = "Julián";
+        int numero1 = 1;
+        float numero2 = 1.25f;
+        double numero3 = 1.52;
+        short numero4 = 5;
+        long numero5 = 100;
+        char caracter = 'a';
+        boolean valorDeVerdad = true;
+
+        System.out.println(nombre);
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(caracter);
+        System.out.println(valorDeVerdad);
+    }
+}
+
